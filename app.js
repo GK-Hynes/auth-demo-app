0960f96eb1ac404adb3420ec26f23d22d1b11e5b
Gerard <gkhynes@gmail.com>
2017-12-22 17:29:34 +0000

    Add register routes and form

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const expressSession = require("express-session");
 
 mongoose.connect("mongodb://localhost/auth_demo", { useMongoClient: true });
+mongoose.Promise = global.Promise;
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   expressSession({
     secret: "Gaeilge abú",
@@ -25,12 +27,40 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// ===============
+// ROUTES
+// ===============
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/secret", (req, res) => {
   res.render("secret");
+});
+
+// AUTH ROUTES
+
+// Show sign up form
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// Handle user sign up
+app.post("/register", (req, res) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        return res.render("register");
+      }
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secret");
+      });
+    }
+  );
 });
 
 app.listen(3000, () => {
