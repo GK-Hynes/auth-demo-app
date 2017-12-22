d83e08803a6ce3f03d563fce54360fc9b48aa0e3
Gerard <gkhynes@gmail.com>
2017-12-22 16:40:31 +0000

    Configure passport

--- app.js
+++ app.js
@@ -5,11 +5,25 @@ const bodyParser = require("body-parser");
 const User = require("./models/user");
 const LocalStrategy = require("passport-local");
 const passportLocalMongoose = require("passport-local-mongoose");
+const expressSession = require("express-session");
 
 mongoose.connect("mongodb://localhost/auth_demo", { useMongoClient: true });
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(
+  expressSession({
+    secret: "Gaeilge abú",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
