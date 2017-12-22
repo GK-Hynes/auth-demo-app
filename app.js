dfb9dc65c48765583205b71c65e8a36a390eae69
Gerard <gkhynes@gmail.com>
2017-12-22 16:22:44 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://localhost/auth_demo", { useMongoClient: true });
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/secret", (req, res) => {
+  res.render("secret");
+});
+
+app.listen(3000, () => {
+  console.log("The server has started.......");
+});
